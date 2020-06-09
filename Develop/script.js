3cb63d584b08ae8e7889d19e2b59930a0920a108
Franamaus <64950097+franamaus@users.noreply.github.com>
2020-06-08 17:44:09 -0700

    used string variables, added prompts, created randomised string

--- Develop/script.js
+++ Develop/script.js
@@ -24,8 +24,6 @@ generateBtn.addEventListener("click", writePassword);
 //   characterUpperCase[i] = characterLowerCase[i].toUpperCase();
 // };
 
-// var characterNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-// var characterSpecial = ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "\\", "[", "]", "^", "_", "`", "{", "}", "|", "~"];
 
 // userInput = [true, false, true, false]
 // var result = ''
@@ -36,17 +34,78 @@ generateBtn.addEventListener("click", writePassword);
 //     result += characterUpperCase;
 // };  
 
-var userSelectionPrompts = function() {
-  var passwordLength = window.prompt("Choose length of password. Must be 8-128 characters long.")
-  if (passwordLength > 8 && passwordLength < 128) {
-    console.log("the user has entered " + passwordLength);
-    return true;
-  } else {
-    alert("You must enter a number between 8-128.");
-    userSelectionPrompts();
+// var userSelectionPrompts = function() {
+//   var passwordLength = window.prompt("Choose length of password. Must be 8-128 characters long.")
+//   if (passwordLength > 8 && passwordLength < 128) {
+//     console.log("the user has entered " + passwordLength);
+//   } else {
+//     alert("You must enter a number between 8-128.");
+//     userSelectionPrompts();
+//   }
+
+  var selectCharacterLength = function() {
+    var passwordLength = window.prompt("Choose length of password. Must be 8-128 characters long.")
+    if (passwordLength < 8 || passwordLength > 128) {
+      alert("You must enter a number between 8-128.");
+      selectCharacterLength();
+    }
+    return passwordLength;
   }
   
-}
+ 
+  
+  
+  function selectCharacterTypes() {
+    var selectionPool = '';
+  var selectLowerCase = window.confirm("Would you like lowercase characters in your password?")
+  console.log(selectLowerCase);
+  if (selectLowerCase == true) {
+    selectionPool += characterLowerCase;
+    console.log("lowercase " + selectLowerCase);
+  }
+
+  var selectUpperCase = window.confirm("Would you like UPPERCASE characters in your password?")
+  console.log(selectUpperCase);
+  if (selectUpperCase == true) {
+    selectionPool += characterUpperCase;
+    console.log("uppercase " + selectUpperCase);
+  }
+
+  var selectNumber = window.confirm("Would you like to have numbers in your password?")
+  console.log(selectNumber);
+  if (selectNumber == true) {
+    selectionPool += characterNumber;
+    console.log("number " + selectNumber);
+  }
+
+  var selectSpecialCharacter = window.confirm("Would you like to have special characters (#@*) in your password?")
+  console.log(selectSpecialCharacter);
+  if (selectSpecialCharacter == true) {
+    selectionPool += characterSpecialCharacter;
+    console.log("special characters " + selectSpecialCharacter);
+  }
+
+  if (selectionPool === '') {
+    alert("You must select at least one or more of the 4 options for character types.")
+    selectCharacterTypes();
+  }
+  return selectionPool;
+  }
+
+
+
+// var userSelectionPrompts = function() {
+//   var passwordLength = window.prompt("Choose length of password. Must be 8-128 characters long.")
+//   if (passwordLength > 8 && passwordLength < 128) {
+//     passwordLength = false;
+//     console.log("the user has entered " + passwordLength);
+//     alert("You must enter a number between 8-128.");
+//     userSelectionPrompts();
+//   } else {
+//     // console.log("the user has entered " + passwordLength);
+//     return passwordLength;
+//   }
+// }
   
 //   var confirmLowerCase = window.confirm("Would you like your password to include lower case letters?");
 //   if (confirmLowerCase) {
@@ -54,7 +113,44 @@ var userSelectionPrompts = function() {
 // }
 // }
 
+var characterLowerCase = 'abcdefghijklmnopqrstuvwxyz';
+var characterUpperCase = characterLowerCase.toUpperCase();
+var characterNumber = '0123456789';
+var characterSpecialCharacter = '!"#$%&()\'*+,-./:<>=?@[]\\^_`{}|~';
+
+// var characterTypeChoice = [
+//   { 
+//     name: "lowercase",
+//     list: randomNumber(0, characterLowerCase.length)
+//   },
+//   {
+//     name: "UPPERCASE",
+//     list: randomNumber(0,characterUpperCase.length)
+//   },
+//   {
+//     name: "Number",
+//     list: randomNumber(0,characterNumber.length)
+//   },
+//   {
+//     name: "Sepcial Characters",
+//     list: randomNumber(0, characterSpecialCharacter.length)
+//   }
+// ];
+
+// for (var i = 0; i < characterTypeChoice.length; i++) {
+
+// }
+
 
 function generatePassword() {
-  userSelectionPrompts();
+  var passwordLength = selectCharacterLength();
+  var chosenCharacterPool = selectCharacterTypes();
+  console.log(passwordLength + " with " + chosenCharacterPool);
+  var password = '';
+  for (i = 0; i < passwordLength; i++) {
+    var index = Math.floor(Math.random() * (chosenCharacterPool.length -1));
+    password += chosenCharacterPool[index];
+  }
+  console.log(password.length);
+return password;
 }
